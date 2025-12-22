dceed3ecf7c8e559b4418d625298d6cc14cd0385
aswinadi <aswin.adisaputra@gmail.com>
2025-12-22 14:24:17 +0700

    fix: add missing remember import

--- app/src/main/java/com/maxmar/attendance/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/maxmar/attendance/ui/screens/home/HomeScreen.kt
@@ -48,6 +48,7 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
